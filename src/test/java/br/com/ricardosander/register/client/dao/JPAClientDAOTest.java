1a64a6a3576878ba77e49782cdae837918c5c3fc
ricardosander <ricardo.sander.lopes@gmail.com>
2018-03-15 03:17:54 -0300

    Add JPAClientDAOTest unit test class.

--- src/test/java/br/com/ricardosander/register/client/dao/JPAClientDAOTest.java
+++ src/test/java/br/com/ricardosander/register/client/dao/JPAClientDAOTest.java
@@ -1,5 +1,46 @@
+package br.com.ricardosander.register.client.dao;
+
+import br.com.ricardosander.register.client.model.Client;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import static org.junit.Assert.*;
 
 public class JPAClientDAOTest {
 
+    EntityManagerFactory emf;
+
+    @Before
+    public void setUp() {
+        emf = Persistence.createEntityManagerFactory("client");
+    }
+
+    @After
+    public void shutdown() {
+        emf.close();
+    }
+
+    @Test
+    public void insert() {
+
+        EntityManager entityManager = emf.createEntityManager();
+
+        JPAClientDAO jpaClientDAO = new JPAClientDAO(entityManager);
+
+        Client client = new Client();
+        client.setCpf("123.456.79.00");
+        client.setNome("Ricardo Sander");
+
+        boolean result = jpaClientDAO.insert(client);
+
+        entityManager.close();
+
+        assertTrue(result);
+        assertNotNull(client.getId());
+    }
 }
